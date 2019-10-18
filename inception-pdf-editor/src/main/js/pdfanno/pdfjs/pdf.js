6798b246926584b832105329a65034a66c224039
nyneshreddy <nygu18@student.bth.se>
2019-10-18 20:36:49 +0200

    changes

--- inception-pdf-editor/src/main/js/pdfanno/pdfjs/pdf.js
+++ inception-pdf-editor/src/main/js/pdfanno/pdfjs/pdf.js
@@ -4217,137 +4217,137 @@ var SVGGraphics = (function SVGGraphicsClosure() {
         var args = opTree[x].args;
 
           switch (fnId | 0) {
-              case OPS.beginText:
-                  this.beginText();
-                  break;
-              case OPS.setLeading:
-                  this.setLeading(args);
-                  break;
-              case OPS.setLeadingMoveText:
-                  this.setLeadingMoveText(args[0], args[1]);
-                  break;
-              case OPS.setFont:
-                  this.setFont(args);
-                  break;
-              case OPS.showText:
-                  this.showText(args[0]);
-                  break;
-              case OPS.showSpacedText:
-                  this.showText(args[0]);
-                  break;
-              case OPS.endText:
-                  this.endText();
-                  break;
-              case OPS.moveText:
-                  this.moveText(args[0], args[1]);
-                  break;
-              case OPS.setCharSpacing:
-                  this.setCharSpacing(args[0]);
-                  break;
-              case OPS.setWordSpacing:
-                  this.setWordSpacing(args[0]);
-                  break;
-              case OPS.setHScale:
-                  this.setHScale(args[0]);
-                  break;
-              case OPS.setTextMatrix:
-                  this.setTextMatrix(args[0], args[1], args[2],
-                      args[3], args[4], args[5]);
-                  break;
-              case OPS.setLineWidth:
-                  this.setLineWidth(args[0]);
-                  break;
-              case OPS.setLineJoin:
-                  this.setLineJoin(args[0]);
-                  break;
-              case OPS.setLineCap:
-                  this.setLineCap(args[0]);
-                  break;
-              case OPS.setMiterLimit:
-                  this.setMiterLimit(args[0]);
-                  break;
-              case OPS.setFillRGBColor:
-                  this.setFillRGBColor(args[0], args[1], args[2]);
-                  break;
-              case OPS.setStrokeRGBColor:
-                  this.setStrokeRGBColor(args[0], args[1], args[2]);
-                  break;
-              case OPS.setDash:
-                  this.setDash(args[0], args[1]);
-                  break;
-              case OPS.setGState:
-                  this.setGState(args[0]);
-                  break;
-              case OPS.fill:
-                  this.fill();
-                  break;
-              case OPS.eoFill:
-                  this.eoFill();
-                  break;
-              case OPS.stroke:
-                  this.stroke();
-                  break;
-              case OPS.fillStroke:
-                  this.fillStroke();
-                  break;
-              case OPS.eoFillStroke:
-                  this.eoFillStroke();
-                  break;
-              case OPS.clip:
-                  this.clip('nonzero');
-                  break;
-              case OPS.eoClip:
-                  this.clip('evenodd');
-                  break;
-              case OPS.paintSolidColorImageMask:
-                  this.paintSolidColorImageMask();
-                  break;
-              case OPS.paintJpegXObject:
-                  this.paintJpegXObject(args[0], args[1], args[2]);
-                  break;
-              case OPS.paintImageXObject:
-                  this.paintImageXObject(args[0]);
-                  break;
-              case OPS.paintInlineImageXObject:
-                  this.paintInlineImageXObject(args[0]);
-                  break;
-              case OPS.paintImageMaskXObject:
-                  this.paintImageMaskXObject(args[0]);
-                  break;
-              case OPS.paintFormXObjectBegin:
-                  this.paintFormXObjectBegin(args[0], args[1]);
-                  break;
-              case OPS.paintFormXObjectEnd:
-                  this.paintFormXObjectEnd();
-                  break;
-              case OPS.closePath:
-                  this.closePath();
-                  break;
-              case OPS.closeStroke:
-                  this.closeStroke();
-                  break;
-              case OPS.closeFillStroke:
-                  this.closeFillStroke();
-                  break;
-              case OPS.nextLine:
-                  this.nextLine();
-                  break;
-              case OPS.transform:
-                  this.transform(args[0], args[1], args[2], args[3],
-                      args[4], args[5]);
-                  break;
-              case OPS.constructPath:
-                  this.constructPath(args[0], args[1]);
-                  break;
-              case OPS.endPath:
-                  this.endPath();
-                  break;
-              case 92:
-                  this.group(opTree[x].items);
-                  break;
-              default:
-                  warn('Unimplemented method '+ fn);
-                  break;
+          case OPS.beginText:
+              this.beginText();
+              break;
+          case OPS.setLeading:
+              this.setLeading(args);
+              break;
+          case OPS.setLeadingMoveText:
+              this.setLeadingMoveText(args[0], args[1]);
+              break;
+          case OPS.setFont:
+              this.setFont(args);
+              break;
+          case OPS.showText:
+              this.showText(args[0]);
+              break;
+          case OPS.showSpacedText:
+              this.showText(args[0]);
+              break;
+          case OPS.endText:
+              this.endText();
+              break;
+          case OPS.moveText:
+              this.moveText(args[0], args[1]);
+              break;
+          case OPS.setCharSpacing:
+              this.setCharSpacing(args[0]);
+              break;
+          case OPS.setWordSpacing:
+              this.setWordSpacing(args[0]);
+              break;
+          case OPS.setHScale:
+              this.setHScale(args[0]);
+              break;
+          case OPS.setTextMatrix:
+              this.setTextMatrix(args[0], args[1], args[2],
+                  args[3], args[4], args[5]);
+              break;
+          case OPS.setLineWidth:
+              this.setLineWidth(args[0]);
+              break;
+          case OPS.setLineJoin:
+              this.setLineJoin(args[0]);
+              break;
+          case OPS.setLineCap:
+              this.setLineCap(args[0]);
+              break;
+          case OPS.setMiterLimit:
+              this.setMiterLimit(args[0]);
+              break;
+          case OPS.setFillRGBColor:
+              this.setFillRGBColor(args[0], args[1], args[2]);
+              break;
+          case OPS.setStrokeRGBColor:
+              this.setStrokeRGBColor(args[0], args[1], args[2]);
+              break;
+          case OPS.setDash:
+              this.setDash(args[0], args[1]);
+              break;
+          case OPS.setGState:
+              this.setGState(args[0]);
+              break;
+          case OPS.fill:
+              this.fill();
+              break;
+          case OPS.eoFill:
+              this.eoFill();
+              break;
+          case OPS.stroke:
+              this.stroke();
+              break;
+          case OPS.fillStroke:
+              this.fillStroke();
+              break;
+          case OPS.eoFillStroke:
+              this.eoFillStroke();
+              break;
+          case OPS.clip:
+              this.clip('nonzero');
+              break;
+          case OPS.eoClip:
+              this.clip('evenodd');
+              break;
+          case OPS.paintSolidColorImageMask:
+              this.paintSolidColorImageMask();
+              break;
+          case OPS.paintJpegXObject:
+              this.paintJpegXObject(args[0], args[1], args[2]);
+              break;
+          case OPS.paintImageXObject:
+              this.paintImageXObject(args[0]);
+              break;
+          case OPS.paintInlineImageXObject:
+              this.paintInlineImageXObject(args[0]);
+              break;
+          case OPS.paintImageMaskXObject:
+              this.paintImageMaskXObject(args[0]);
+              break;
+          case OPS.paintFormXObjectBegin:
+              this.paintFormXObjectBegin(args[0], args[1]);
+              break;
+          case OPS.paintFormXObjectEnd:
+              this.paintFormXObjectEnd();
+              break;
+          case OPS.closePath:
+              this.closePath();
+              break;
+          case OPS.closeStroke:
+              this.closeStroke();
+              break;
+          case OPS.closeFillStroke:
+              this.closeFillStroke();
+              break;
+          case OPS.nextLine:
+              this.nextLine();
+              break;
+          case OPS.transform:
+              this.transform(args[0], args[1], args[2], args[3],
+                  args[4], args[5]);
+              break;
+          case OPS.constructPath:
+              this.constructPath(args[0], args[1]);
+              break;
+          case OPS.endPath:
+              this.endPath();
+              break;
+          case 92:
+              this.group(opTree[x].items);
+              break;
+          default:
+              warn('Unimplemented method '+ fn);
+              break;
           }
       }
     },
